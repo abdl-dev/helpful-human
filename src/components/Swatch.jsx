2f582940a6a5b4adf3bef489772fad1740648ee5
abdul-malek-dev <me@abdul-malek.dev>
2021-07-02 23:04:09 -0400

    make swatches responsive

--- src/components/Swatch.jsx
+++ src/components/Swatch.jsx
@@ -1,7 +1,9 @@
 const Swatch = ({hex_code}) => {
 	return (
-		<div style={{ backgroundColor: hex_code }} className="swatch">
-			<label htmlFor="swatch">{hex_code}</label>
+		<div className="col-3">
+			<div style={{ backgroundColor: hex_code }} className="swatch">
+				<label htmlFor="swatch">{hex_code}</label>
+			</div>
 		</div>
 	)
 }
--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -2,12 +2,19 @@ import Swatch from './Swatch'
 
 const Viewer = () => {
 	return (
-		<div className="col-9 view">
+		<div className="col-9 viewer">
 			<Swatch hex_code="red" />
-			<Swatch hex_code="blue" />
-			<Swatch hex_code="green" />
-			<Swatch hex_code="yellow" />
 			<Swatch hex_code="orange" />
+			<Swatch hex_code="yellow" />
+			<Swatch hex_code="green" />
+			<Swatch hex_code="blue" />
+			<Swatch hex_code="purple" />
+			<Swatch hex_code="brown" />
+			<Swatch hex_code="gray" />
+
+			<Swatch hex_code="red" />
+			<Swatch hex_code="orange" />
+			<Swatch hex_code="yellow" />
 			<Swatch hex_code="green" />
 		</div>
 	)
