b7bd0c42273c38b1132d57cbc920e52580de0610
abdul-malek-dev <me@abdul-malek.dev>
2021-07-03 13:34:17 -0400

    add pagination and styling changes

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # helpful-human 
 
 Interview challenge for Helpful Human
+
+## TODO
+
+- Look through all files for semicolon consistency
--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -1,21 +1,39 @@
 import { useState } from 'react'
+import ReactPaginate from 'react-paginate'
 import Swatch from './Swatch'
 
 const Viewer = () => {
 	const [viewer, setViewer] = useState(null)
-	let elements = []
+
+	let random_hex = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+	let elements = [];
+	let pageSize = 12;
 
 	for (let i = 0; i < 120; i++) {
 		let color = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
 		elements.push(<Swatch key={ color } hex_code={ color }/>)
 	}
 
-	let random_hex = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+	let numPages = Math.ceil(elements.length / pageSize);
+
+	let currPageNum = 0;
+	let currPage = elements.slice(pageSize * currPageNum, pageSize * (currPageNum + 1));
 
 	return (
+		<>
 		<div className="col-9 viewer">
-			{ elements }
+			<div className="col-12 page">
+				{ currPage }>
+			</div>
+			<div className="col-12 paginate">
+				<ReactPaginate>
+					previousLabel={ 'Previous' }
+					nextLabel={ 'Next' }
+					pageCount={ numPages }
+				</ReactPaginate>
+			</div>
 		</div>
+		</>
 	)
 }
 
