cbc8af32626083003e04456feb4d175252137e0f
abdul-malek-dev <me@abdul-malek.dev>
2021-07-02 23:27:00 -0400

    add swatch pointer on hover and onClick boilerplate

--- src/components/Swatch.jsx
+++ src/components/Swatch.jsx
@@ -1,7 +1,12 @@
+import { useState } from 'react'
+
 const Swatch = ({hex_code}) => {
+	const onClick = (info) => {
+		console.log(info);
+	}
 	return (
 		<div className="col-3">
-			<div style={{ backgroundColor: hex_code }} className="swatch">
+			<div style={{ backgroundColor: hex_code }} className="swatch" onClick={onClick}>
 				<label htmlFor="swatch">{hex_code}</label>
 			</div>
 		</div>
